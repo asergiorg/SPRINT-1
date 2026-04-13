4e68038af7a0605c3dda99134b078ebce1826001
Ángel Sergio Reyes Guedes <asergiorg2005@gmail.com>
2026-04-13 10:51:54 +0100

    Added reservation information to app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Index } from './pages/index/index';
 import { ActivictiesCatalog } from './pages/activicties-catalog/activicties-catalog';
 import { UserActivities } from './pages/user-activities/user-activities';
+import { ReservationInformation } from './pages/reservation-information/reservation-information';
 
 export const routes: Routes = [
     {path: '', component: Index},
     {path: 'activities', component: ActivictiesCatalog},
-    { path: 'user-activities', component: UserActivities }
+    { path: 'user-activities', component: UserActivities },
+    { path: 'reservation-information/:id', component: ReservationInformation }
 ];
 
