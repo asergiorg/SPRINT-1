a24704b395f84dd7d632718fc13beb54d46da4e5
Ángel Sergio Reyes Guedes <asergiorg2005@gmail.com>
2026-04-28 23:59:07 +0100

    Modified version of the project to include firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,38 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+
+import { routes } from './app.routes';
+
+// Firebase Imports
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: ".firebaseapp.com",
+  projectId: "",
+  storageBucket: ".appspot.com",
+  messagingSenderId: "",
+  appId: ""
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+
+    provideFirestore(() => getFirestore()),
+
+    provideAuth(() => getAuth()),
   ]
 };
+
+
+
