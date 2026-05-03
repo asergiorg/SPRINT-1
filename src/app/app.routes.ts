8f20e58a93138f877aa80080c6d34db01b044b7b
UbayBatista <ubayantonio@gmail.com>
2026-05-03 22:21:25 +0200

    Update dependencies, routing and root configurations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { Index } from './pages/index/index';
-import { ActivictiesCatalog } from './pages/activicties-catalog/activicties-catalog';
+import { ActivitiesCatalog } from './pages/activities-catalog/activities-catalog';
 import { UserActivities } from './pages/user-activities/user-activities';
 import { ReservationInformation } from './pages/reservation-information/reservation-information';
 import { ActivityInformation } from './pages/activity-information/activity-information';
 
 export const routes: Routes = [
-    {path: 'activities', component: ActivictiesCatalog},
+    {path: 'activities', component: ActivitiesCatalog},
     { path: 'user-activities', component: UserActivities },
     { path: 'activity-information/:id', component: ActivityInformation },
     { path: 'reservation-information/:id', component: ReservationInformation },
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,53 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, PLATFORM_ID, inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./components/footer/footer";
+import { Header } from "./components/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('GrandAventures');
+  
+  private platformId = inject(PLATFORM_ID);
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.initializeTestUsers();
+    }
+  }
+
+  async initializeTestUsers() {
+    try {
+        const response = await fetch('/assets/data/users.json');
+        
+        if (response.ok) {
+            const testUsers = await response.json();
+            
+            let existingUsers = JSON.parse(localStorage.getItem('users') || '[]');
+            let modified = false;
+
+            testUsers.forEach((testUser: any) => {
+                const exists = existingUsers.find((u: any) => u.username === testUser.username);
+                
+                if (!exists) {
+                    existingUsers.push(testUser);
+                    modified = true;
+                }
+            });
+
+            if (modified) {
+                localStorage.setItem('users', JSON.stringify(existingUsers));
+                console.log("Usuarios de prueba cargados y fusionados con éxito.");
+            }
+        }
+    } catch (error) {
+        console.error("Error al cargar los usuarios de prueba: ", error);
+    }
+  }
 }
