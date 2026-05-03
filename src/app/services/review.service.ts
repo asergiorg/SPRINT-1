b8249332b2edc4f97f4451101cf1fd648b7131a6
Ángel Sergio Reyes Guedes <asergiorg2005@gmail.com>
2026-05-03 22:32:55 +0100

    Refactorized code

--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -8,9 +8,7 @@ import { HttpClient } from '@angular/common/http';
 export class ReviewService {
   private firestore = inject(Firestore);
   private collectionName = 'reviews';
-
   constructor(private http: HttpClient) {}
-
   async addReview(data: ReviewData): Promise<string> {
     try {
       const ref = collection(this.firestore, this.collectionName);
@@ -21,27 +19,10 @@ export class ReviewService {
       throw error;
     }
   }
-
   getReviews(): Observable<any> {
     const ref = collection(this.firestore, this.collectionName);
     return collectionData(ref, { idField: 'id' });
   }
-
-  getReviewById(id: string): Observable<any> {
-    const ref = doc(this.firestore, `${this.collectionName}/${id}`);
-    return docData(ref, { idField: 'id' });
-  }
-
-  async updateReview(id: string, data: any): Promise<void> {
-    try {
-      const ref = doc(this.firestore, `${this.collectionName}/${id}`);
-      await updateDoc(ref, data);
-    } catch (error) {
-      console.error('Error al actualizar Review:', error);
-      throw error;
-    }
-  }
-
   async uploadToCloudinary(file: File): Promise<string> {
     const cloudName = 'dro3xapg0';
     const uploadPreset = 'reseñas';
