434e9ab1a396fa6c4b5129fd8c1f03bb2a8b3b7b
Kilian Santana Delgado <kiliandelgado2113@gmail.com>
2026-05-03 20:32:24 +0100

    Firebase credentials

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,21 +2,19 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-
 import { routes } from './app.routes';
-
-// Firebase Imports
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: ".firebaseapp.com",
-  projectId: "",
-  storageBucket: ".appspot.com",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBH9L9_Qo2sSGu4wsC0jCEv0ojIBoQvaq4",
+  authDomain: "grand-aventures-8b3d9.firebaseapp.com",
+  projectId: "grand-aventures-8b3d9",
+  storageBucket: "grand-aventures-8b3d9.firebasestorage.app",
+  messagingSenderId: "766049528697",
+  appId: "1:766049528697:web:edbbe6fd283a07c273dda8",
+  measurementId: "G-Q2FSLCWG2B"
 };
 
 export const appConfig: ApplicationConfig = {
