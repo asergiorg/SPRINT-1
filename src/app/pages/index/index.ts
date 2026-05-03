09df1096094a773a3e9dea47e54c5ba980191b46
Kilian Santana Delgado <kiliandelgado2113@gmail.com>
2026-05-03 11:48:44 +0100

    Logic of search bar on home page

--- src/app/pages/index/index.ts
+++ src/app/pages/index/index.ts
@@ -3,20 +3,22 @@ import { Activity1 } from '../../components/activity1/activity1';
 import { Header } from '../../components/header/header';
 import { Activity } from '../../models/activity.model';
 import { ActivityService } from '../../services/activity.service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [Activity1, Header, RouterLink],
+  imports: [Activity1, Header, RouterLink, FormsModule],
   templateUrl: './index.html',
   styleUrl: './index.css',
 })
 export class Index implements OnInit{
   activities: Activity[] = [];
+  searchText: string = '';
 
-  constructor(private activitiesService: ActivityService){}
+  constructor(private activitiesService: ActivityService, private router: Router){}
 
   ngOnInit(): void {
     this.activitiesService.getActivities().subscribe(data => {
@@ -26,5 +28,11 @@ export class Index implements OnInit{
 
   onActivityClicked(activityId: number) {
     console.log('Activity clicked:', activityId);
+  }
+
+  onSearch(){
+    if(this.searchText.trim() !== '') {
+      this.router.navigate(['/activities'], { queryParams: { search: this.searchText } });
+    }
   }
 }
